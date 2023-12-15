9331f70962df5dbe36243a96cc21a84cdd263506
Giovani Eugenio <giovani.eugenio.sp@gmail.com>
2023-12-15 11:54:41 -0300

    commit único

--- cadastro-pessoas-springboot/src/main/java/br/com/cadastropessoas/controllers/GreetingsController.java
+++ cadastro-pessoas-springboot/src/main/java/br/com/cadastropessoas/controllers/GreetingsController.java
@@ -1,58 +1,24 @@
 package br.com.cadastropessoas.controllers;
 
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import br.com.cadastropessoas.model.Usuario;
 import br.com.cadastropessoas.repository.UsuarioRepository;
 
-/**
- *
- * A sample greetings controller to return greeting text
- */
 @RestController
 public class GreetingsController {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-	
-	
-    /**
-     *
-     * @param name the name to greet
-     * @return greeting text
-     */
-
-    @RequestMapping(value = "/mostrarnome/{name}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String greetingText(@PathVariable String name) {
-        return "Hello " + name + "!";
-    }
-
-    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
-    @ResponseStatus(HttpStatus.OK)
-    public String retornaOlamundo(@PathVariable String nome) {
-    	
-    	Usuario usuario = new Usuario();
-    	usuario.setNome(nome);
-    	usuarioRepository.save(usuario);
-    	
-    	return "Olá mundo "+nome+"!";
-    }
     
     @GetMapping(value = "listatodos") 
     @ResponseBody 
